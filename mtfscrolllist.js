9507245f6855dd75c94b1897b644f8214c88f9ac
馒头饭 <mhjlw@126.com>
2021-01-09 22:06:21 +0800

    :new:增加缓存高度栈

--- mtfscrolllist.js
+++ mtfscrolllist.js
@@ -4,35 +4,66 @@ const init = ({ele = null, data = [], tpl = v => v, startIndex = 0, perPage = da
     Dom.append(data)
     scrollListener()
 }
+class cacheHeightStack {
+    constructor () {
+        this.stack = []
+        this.height = 0
+    }
+    isEmpty() {
+        return this.stack.length === 0
+    }
+    push ({ele, height}) {
+        this.height += height || 0
+        return this.stack.push({ele, height})
+    }
+    front () {
+        return this.stack[this.stack.length - 1]
+    }
+    pop () {
+        const r = this.stack.pop()
+        this.height -= r.height || 0
+        return r
+    }
+}
 const Dom = {
-    cache: [[], []],
+    cache: { top: new cacheHeightStack, bottom: new cacheHeightStack },
+    height (ele) {
+        const styles = ele.currentStyle || window.getComputedStyle(ele)
+        return ele.offsetHeight + parseInt(styles['marginTop'] | 0) + parseInt(styles['marginBottom'] | 0)
+    },
     prepend (data) {
-        const firstChild = G.ele.firstChild
+        const firstChild = G.ele.firstChild 
         data.forEach(data => G.ele.insertBefore(G.tpl(data, G.startIndex++), firstChild))
     },
     prependFromCache () {
-        const firstChild = G.ele.firstChild, prev = Dom.cache[0].pop()
-        if (prev) prev.forEach(n => G.ele.insertBefore(n, firstChild))
+        const firstChild = G.ele.firstChild, prev = Dom.cache.top.pop()
+        if (prev) prev.ele.forEach(ele => G.ele.insertBefore(ele, firstChild))
     },
     append (data) {
         data.forEach(data => G.ele.appendChild(G.tpl(data, G.startIndex++)))
     },
     appendFromCache () {
-        const last = Dom.cache[1].pop()
-        if (last) last.forEach(n => G.ele.appendChild(n))
+        const last = Dom.cache.bottom.pop()
+        if (last) last.ele.forEach(ele => G.ele.appendChild(ele))
     },
     clearTop () {
         if (G.ele.children.length > G.perPage) {
-            let i = -1, t = Array(G.perPage)
-            while (++i < G.perPage) G.ele.removeChild(t[i] = G.ele.children[0])
-            Dom.cache[0].push(t)
+            let i = -1, ele = Array(G.perPage), height = 0
+            while (++i < G.perPage) {
+                height += Dom.height(ele[i] = G.ele.children[0])
+                G.ele.removeChild(ele[i])
+            }
+            Dom.cache.top.push({ele, height})
         }
     },
     clearBottom () {
         if (G.ele.children.length > G.perPage) {
-            let i = G.perPage, t = Array(G.perPage)
-            while (i--) G.ele.removeChild(t[i] = G.ele.children[G.ele.children.length - 1])
-            Dom.cache[1].push(t)
+            let i = G.perPage, ele = Array(G.perPage), height = 0
+            while (i--) {
+                height += Dom.height(ele[i] = G.ele.children[G.ele.children.length - 1])
+                G.ele.removeChild(ele[i])
+            }
+            Dom.cache.bottom.push({ele, height})
         }
     }
 }
@@ -41,20 +72,26 @@ const scrollListener = () => {
     if (typeof IntersectionObserver === void 0) {
 
     } else {
+        const reObserve = (observer, ele) => {
+            observer.disconnect()
+            observer.observe(ele)
+        }
         const topObserver = new IntersectionObserver((entries, observer) => {
             if (entries[0].isIntersecting) {
                 Dom.clearBottom()
                 bottomObserver.disconnect()
                 bottomObserver.observe(G.ele.lastChild)
-
-                if (Dom.cache[1].length === 0) {
+                if (Dom.cache.top.isEmpty()) {
                     const data = G.onTop()
-                    if (data) Dom.prepend(data)
+                    if (data) {
+                        Dom.prepend(data)
+                        reObserve(observer, G.ele.firstChild)
+                    }
                 } else {
                     Dom.prependFromCache()
+                    reObserve(observer, G.ele.firstChild)
                 }
-                observer.disconnect()
-                observer.observe(G.ele.firstChild)
+                
             }
         }, { root: G.ele, threshold: 0 })
         const bottomObserver = new IntersectionObserver((entries, observer) => {
@@ -62,15 +99,16 @@ const scrollListener = () => {
                 Dom.clearTop()
                 topObserver.disconnect()
                 topObserver.observe(G.ele.firstChild)
-
-                if (Dom.cache[1].length === 0) {
+                if (Dom.cache.bottom.isEmpty()) {
                     const data = G.onBottom()
-                    if (data) Dom.append(data)
+                    if (data) {
+                        Dom.append(data)
+                        reObserve(observer, G.ele.lastChild)
+                    }
                 } else {
                     Dom.appendFromCache()
+                    reObserve(observer, G.ele.lastChild)
                 }
-                observer.disconnect()
-                observer.observe(G.ele.lastChild)
             }
         }, { root: G.ele, threshold: 0 })
         bottomObserver.observe(G.ele.lastChild)
